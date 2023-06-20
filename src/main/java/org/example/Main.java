f0f413ce2a53149c79906c18855644d85c1dd43d
Vrba Martin <Martin.Vrba@t-mobile.cz>
2023-06-20 15:33:26 +0200

    cviceni 2-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car mojeAuto = new Car("Opel", "Astra H", 2006);
+        System.out.println("Driv jsem mel " + mojeAuto.getMake() + " " + mojeAuto.getModel() + " vyrobeny " + mojeAuto.getYear());
+        mojeAuto.setModel("Astra K");
+        mojeAuto.setYear(2019);
+        System.out.println("Ted mam " + mojeAuto.getMake() + " " + mojeAuto.getModel() + " vyrobeny " + mojeAuto.getYear());
+
+        Rectangle obda = new Rectangle(12, 9);
+        double ploska = obda.getArea();
+        System.out.println("Plocha naseho obdelniku je " + ploska);
     }
 }
